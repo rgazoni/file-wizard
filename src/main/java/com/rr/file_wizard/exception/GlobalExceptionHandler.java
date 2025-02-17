b0f03c0df26d6769d4328b9a16fce619be3b93a3
Ramon Gazoni <rgazoni@Ramons-MacBook-Air.local>
2025-02-17 12:26:46 -0300

    Adding transaction support to an upload of a file

--- src/main/java/com/rr/file_wizard/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rr/file_wizard/exception/GlobalExceptionHandler.java
@@ -22,6 +22,11 @@ public class GlobalExceptionHandler {
         return buildErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(FileUploadException.class)
+    public ResponseEntity<Map<String, Object>> handleFileUploadException(FileUploadException ex) {
+        return buildErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     private ResponseEntity<Map<String, Object>> buildErrorResponse(String message, HttpStatus status) {
         Map<String, Object> errorBody = new HashMap<>();
         errorBody.put("timestamp", LocalDateTime.now());
--- src/main/java/com/rr/file_wizard/service/FileMetadataService.java
+++ src/main/java/com/rr/file_wizard/service/FileMetadataService.java
@@ -1,10 +1,12 @@
 package com.rr.file_wizard.service;
 
+import com.rr.file_wizard.exception.FileUploadException;
 import com.rr.file_wizard.exception.FileValidationException;
 import com.rr.file_wizard.model.FileMetadata;
 import com.rr.file_wizard.repository.FileMetadataRepository;
 import com.rr.file_wizard.util.ChecksumUtilImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -41,21 +43,18 @@ public class FileMetadataService {
         return fileMetadata;
     }
 
+    @Transactional(rollbackFor = IOException.class)
     public String uploadFile(MultipartFile file) {
 
         if (file == null || file.isEmpty()) {
             throw new FileValidationException("File must not be null or empty");
         }
 
-        //TODO: Transaction kind of style here in case of one of them fails
-        // ACIDify it.
-        FileMetadata fileMetadata = saveMetadata(file);
-
         try {
+            FileMetadata fileMetadata = saveMetadata(file);
             return s3Service.uploadFile(fileMetadata.getBucketFileName(), file);
         } catch (IOException e) {
-            throw new RuntimeException("Failed to upload file: " + file.getOriginalFilename(), e);
+            throw new FileUploadException("Failed to upload file. Please try again.");
         }
-
     }
 }
